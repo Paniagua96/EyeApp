d3cb075814253405170fb329541ca39ddad1b41c
RoyRenaud <rorenaudro@ucq.edu.mx>
2019-05-14 16:11:22 -0500

    Se agregaron las imagenes al proyecto.

---
+++ Assets/Scripts/ManagerDaltonism.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class ManagerDaltonism : MonoBehaviour
+{
+    public GameObject[] imageTest;
+    public ManagerTest managerTest;
+
+    public void appearTest(int question)
+    {
+        imageTest[question].SetActive(false);
+        imageTest[question + 1].SetActive(true);
+    }
+}
